8122e40008621d5e4cc1430b1aabb18c4c74767b
Oswaldo Gomez <oswaldo.gomez@bsdenterprise.com>
2017-12-13 15:00:34 -0600

    se agrega about

--- app/src/main/java/app/oswaldogh/plazashop/Ui/Main/MainActivity.java
+++ app/src/main/java/app/oswaldogh/plazashop/Ui/Main/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
